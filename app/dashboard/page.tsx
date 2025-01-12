c4ade71cf5077ce9c21080105e459d7510cae142
Anurag Khobragae <anuragk2204@gmail.com>
2025-01-13 00:21:41 +0530

    middle

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import {lusitana} from '@/app/ui/fonts'
-
-export default function Page(){
-    return (
-        <>
-        <p className={`${lusitana.className}`}>Dashboard page</p>
-        </>
-    )
-}
