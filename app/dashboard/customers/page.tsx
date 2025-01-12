853c7ba95ec2dfca2c6d412049ab676d85c80d11
Anurag Khobragae <anuragk2204@gmail.com>
2025-01-12 15:27:05 +0530

    mid

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import {lusitana} from '@/app/ui/fonts'
+
+export default function Page(){
+    return (
+        <>
+        <p className={`${lusitana.className} fixed ml-12`}>customers page</p>
+        </>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import {lusitana} from '@/app/ui/fonts'
+
+export default function Page(){
+    return (
+        <>
+        <p className={`${lusitana.className}`}>Dashboard page</p>
+        </>
+    )
+}
